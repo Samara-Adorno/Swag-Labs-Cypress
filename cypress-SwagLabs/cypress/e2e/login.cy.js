b84fcf9cc9a9f9c5878091e7b38533e7c76850d2
Samara-Adorno <adornosamara165@gmail.com>
2025-11-05 22:23:53 -0300

    finalizando os testes do arquivo de login

--- cypress-SwagLabs/cypress/e2e/login.cy.js
+++ cypress-SwagLabs/cypress/e2e/login.cy.js
@@ -1,7 +1,7 @@
 describe('Funcionalidade de Login - Swag Labs', () => {
 
     // Login com credenciais válidas
-    it('Login com credenciais válidas', () => {
+    it('Dado que quero realizar o login no site, Quando eu digitar as credenciais válidas, Então devo ser redirecionado para a página de produtos', () => {
         const username = 'standard_user';
         const password = 'secret_sauce';
 
@@ -12,14 +12,38 @@ describe('Funcionalidade de Login - Swag Labs', () => {
     })
 
     // Login com senha inválida
-    it('Login com senha inválida', () => {
+    it('Dado que quero logar no sistema, Quando eu digitar uma senha inválida, Então deve exibir uma mensagem de erro', () => {
         const username = 'standard_user';
-        const password = 123456;
+        const password = '123456';
 
         cy.visit('https://www.saucedemo.com/')
         cy.get('.login-box input[name="user-name"] ').type(username, {force: true})
         cy.get('.login-box input[name="password"] ').type(password, {force: true})
         cy.get('.login-box input[name="login-button"]').contains('Login').click()
-        // cy.contains('.login-box .error-message-container').should('be.visible')
+        cy.get('[data-test="error"]').should('be.visible').and('contain.text', 'Epic sadface: Username and password do not match any user in this service')
+    })
+
+    // Login com usuário bloqueado
+    it('Dado que quero logar no sistema, Quando eu digitar um usuário bloqueado, Então deve exibir uma mensagem de erro', () => {
+        const username = 'locked_out_user';
+        const password = 'secret_sauce';
+
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('.login-box input[name="user-name"] ').type(username, {force: true})
+        cy.get('.login-box input[name="password"] ').type(password, {force: true})
+        cy.get('.login-box input[name="login-button"]').contains('Login').click()
+        cy.get('[data-test="error"]').should('be.visible').and('contain.text', 'Epic sadface: Sorry, this user has been locked out.')
+    })
+
+    // Login com campos vazios
+    it('Dado que quero logar no sistema, Quando eu tentar acessar o sistema com o campos vazios, Então deve impedir login e exibir alerta', () => {
+        const username = '';
+        const password = '';
+
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('.login-box input[name="user-name"] ').type(username, {force: true})
+        cy.get('.login-box input[name="password"] ').type(password, {force: true})
+        cy.get('.login-box input[name="login-button"]').contains('Login').click()
+        cy.get('[data-test="error"]').should('be.visible').and('contain.text', 'Epic sadface: Username is required')
     })
 });
